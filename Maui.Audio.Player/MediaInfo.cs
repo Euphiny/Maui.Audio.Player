c1490b9ac7cb0a816b029095115e92b7671ca63c
joh-an-ng <me@johan.ng>
2025-04-09 19:27:51 +0200

    add duration as double to media info

--- Maui.Audio.Player/MediaInfo.cs
+++ Maui.Audio.Player/MediaInfo.cs
@@ -1,8 +1,10 @@
 namespace Maui.Audio.Player;
 
-public class MediaInfo(string title, string artist)
+public class MediaInfo(string title, string artist, double duration)
 {
     public string Title { get; set; } = title;
 
     public string Artist { get; set; } = artist;
+    
+    public double Duration { get; set; }
 }
