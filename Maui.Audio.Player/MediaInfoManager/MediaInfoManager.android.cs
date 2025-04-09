58a89c8b0f09002ca2d8b0e5ffd3a5b87b9837f1
joh-an-ng <me@johan.ng>
2025-04-09 18:46:35 +0200

    add android media info manager file

---
+++ Maui.Audio.Player/MediaInfoManager/MediaInfoManager.android.cs
@@ -0,0 +1,9 @@
+namespace Maui.Audio.Player.MediaInfoManager;
+
+public partial class MediaInfoManager : IMediaInfoManager
+{
+    public void SetMediaInfo(MediaInfo mediaInfo)
+    {
+        throw new NotImplementedException();
+    }
+}
