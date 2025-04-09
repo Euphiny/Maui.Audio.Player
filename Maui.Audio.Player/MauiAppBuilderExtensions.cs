f339d20853a8d4f8234a9f04376f2ac5aca1fb07
joh-an-ng <me@johan.ng>
2025-04-09 19:20:06 +0200

    initialize ios when adding maui app player in project

--- Maui.Audio.Player/MauiAppBuilderExtensions.cs
+++ Maui.Audio.Player/MauiAppBuilderExtensions.cs
@@ -1,3 +1,6 @@
+#if IOS
+    using AVFoundation;
+#endif
 using Maui.Audio.Player.MediaInfoManager;
 
 namespace Maui.Audio.Player;
@@ -7,6 +10,14 @@ public static class MauiAppBuilderExtensions
     public static MauiAppBuilder UseMauiAudioPlayer(this MauiAppBuilder builder)
     {
         builder.Services.AddTransient<IMediaInfoManager, MediaInfoManager.MediaInfoManager>();
+        
+        #if IOS
+        // TODO: move this to a proper initialization class or something
+            var audioSession = AVAudioSession.SharedInstance();
+        
+            audioSession.SetCategory(AVAudioSessionCategory.Playback);
+            audioSession.SetActive(true);
+        #endif
         
         return builder;
     }
