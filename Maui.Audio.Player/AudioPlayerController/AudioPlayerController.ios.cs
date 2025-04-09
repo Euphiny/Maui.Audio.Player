b910c28b105a959cbc4e0db8a4004af016ca0131
joh-an-ng <me@johan.ng>
2025-04-09 20:03:46 +0200

    add start method to audio player controller

--- Maui.Audio.Player/AudioPlayerController/AudioPlayerController.ios.cs
+++ Maui.Audio.Player/AudioPlayerController/AudioPlayerController.ios.cs
@@ -2,5 +2,8 @@ namespace Maui.Audio.Player.AudioPlayerController;
 
 public partial class AudioPlayerController : IAudioPlayerController
 {
-    
+    public void Start(string url, double duration)
+    {
+        throw new NotImplementedException();
+    }
 }
