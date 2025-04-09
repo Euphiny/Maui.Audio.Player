ae9610a4f1509dd54ba19ab84de47b79beb94746
joh-an-ng <me@johan.ng>
2025-04-09 19:29:59 +0200

    create small implementation of set media info ios

--- Maui.Audio.Player/MediaInfoManager/MediaInfoManager.ios.cs
+++ Maui.Audio.Player/MediaInfoManager/MediaInfoManager.ios.cs
@@ -1,9 +1,18 @@
+using MediaPlayer;
+
 namespace Maui.Audio.Player.MediaInfoManager;
 
 public partial class MediaInfoManager : IMediaInfoManager
 {
     public void SetMediaInfo(MediaInfo mediaInfo)
     {
-        throw new NotImplementedException();
+        MPNowPlayingInfoCenter.DefaultCenter.NowPlaying = new MPNowPlayingInfo
+        {
+            Title = mediaInfo.Title,
+            Artist = mediaInfo.Artist,
+            PlaybackDuration = mediaInfo.Duration,
+            ElapsedPlaybackTime = 0,
+            PlaybackRate = 1.0f
+        };
     }
 }
