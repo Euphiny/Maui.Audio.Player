198ee91fd10751cd6cb3311ee7470b168e8ad35a
joh-an-ng <me@johan.ng>
2025-04-10 18:31:29 +0200

    implement start method of ios audio player controller

--- Maui.Audio.Player/AudioPlayerController/AudioPlayerController.ios.cs
+++ Maui.Audio.Player/AudioPlayerController/AudioPlayerController.ios.cs
@@ -1,9 +1,23 @@
+using Maui.Audio.Player.AudioPlayer;
+using Maui.Audio.Player.MediaInfoManager;
+
 namespace Maui.Audio.Player.AudioPlayerController;
 
 public partial class AudioPlayerController : IAudioPlayerController
 {
+    private readonly IMediaInfoManager _mediaInfoManager;
+    private IAudioPlayer? _player;
+
+    public AudioPlayerController(IMediaInfoManager mediaInfoManager)
+    {
+        _mediaInfoManager = mediaInfoManager;
+    }
+    
     public void Start(string url, MediaInfo mediaInfo)
     {
-        throw new NotImplementedException();
+        _player = new AudioPlayer.AudioPlayer(url, mediaInfo.Duration);
+        _mediaInfoManager.SetMediaInfo(mediaInfo);
+        
+        _player.Play();
     }
 }
--- Maui.Audio.Player/MauiAppBuilderExtensions.cs
+++ Maui.Audio.Player/MauiAppBuilderExtensions.cs
@@ -1,3 +1,4 @@
+using Maui.Audio.Player.AudioPlayerController;
 using Maui.Audio.Player.MediaInfoManager;
 
 namespace Maui.Audio.Player;
@@ -7,6 +8,7 @@ public static class MauiAppBuilderExtensions
     public static MauiAppBuilder UseMauiAudioPlayer(this MauiAppBuilder builder)
     {
         builder.Services.AddTransient<IMediaInfoManager, MediaInfoManager.MediaInfoManager>();
+        builder.Services.AddTransient<IAudioPlayerController, AudioPlayerController.AudioPlayerController>();
         
         return builder;
     }
