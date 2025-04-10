644fd43a5656f2342490f69354ae7696a81a765a
joh-an-ng <me@johan.ng>
2025-04-10 19:43:30 +0200

    actually map duration in media info

--- Maui.Audio.Player/MediaInfo.cs
+++ Maui.Audio.Player/MediaInfo.cs
@@ -5,6 +5,6 @@ public class MediaInfo(string title, string artist, double duration)
     public string Title { get; set; } = title;
 
     public string Artist { get; set; } = artist;
-    
-    public double Duration { get; set; }
+
+    public double Duration { get; set; } = duration;
 }
