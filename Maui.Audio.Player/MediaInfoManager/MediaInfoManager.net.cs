d00a71d9d30d0131daf4494184d756c9b68684e4
joh-an-ng <me@johan.ng>
2025-04-10 20:39:15 +0200

    do not throw exception in .net media info manager implementation

--- Maui.Audio.Player/MediaInfoManager/MediaInfoManager.net.cs
+++ Maui.Audio.Player/MediaInfoManager/MediaInfoManager.net.cs
@@ -2,23 +2,11 @@ namespace Maui.Audio.Player.MediaInfoManager;
 
 public partial class MediaInfoManager : IMediaInfoManager
 {
-    public void Initialize()
-    {
-        throw new NotImplementedException();
-    }
+    public void Initialize() { }
 
-    public void SetMediaInfo(MediaInfo mediaInfo)
-    {
-        throw new NotImplementedException();
-    }
+    public void SetMediaInfo(MediaInfo mediaInfo) { }
 
-    public void SetPauseCommand(Action action)
-    {
-        throw new NotImplementedException();
-    }
+    public void SetPauseCommand(Action action) { }
 
-    public void SetPlayCommand(Action action)
-    {
-        throw new NotImplementedException();
-    }
+    public void SetPlayCommand(Action action) { }
 }
