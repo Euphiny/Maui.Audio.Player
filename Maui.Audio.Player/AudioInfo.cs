561eadcb270a14b4f8b1dddcff48f377bc521f1a
joh-an-ng <me@johan.ng>
2025-04-10 21:31:49 +0200

    delete audio info class

--- Maui.Audio.Player/AudioInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace Maui.Audio.Player;
-
-public class AudioInfo(string title, double durationInSeconds)
-{
-    public string Title { get; set; } = title;
-    
-    public double DurationInSeconds { get; set; } = durationInSeconds;
-}
