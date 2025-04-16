ad3d3f8489798ff277fe8fedb75eebbca67b9bd8
joh-an-ng <me@johan.ng>
2025-04-16 18:59:47 +0200

    do not throw not implemented exception in android implementation of MediaInfoManager

--- Maui.Audio.Player/MediaInfoManager/MediaInfoManager.android.cs
+++ Maui.Audio.Player/MediaInfoManager/MediaInfoManager.android.cs
@@ -4,31 +4,31 @@ public partial class MediaInfoManager : IMediaInfoManager
 {
     public void Initialize()
     {
-        throw new NotImplementedException();
+        
     }
 
     public void SetMediaInfo(MediaInfo mediaInfo)
     {
-        throw new NotImplementedException();
+        
     }
 
     public void SetPauseCommand(Action action)
     {
-        throw new NotImplementedException();
+        
     }
 
     public void SetPlayCommand(Action action)
     {
-        throw new NotImplementedException();
+        
     }
 
     public void SetNextCommand(Action action)
     {
-        throw new NotImplementedException();
+        
     }
 
     public void SetPreviousCommand(Action action)
     {
-        throw new NotImplementedException();
+        
     }
 }
