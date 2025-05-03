d6984c8fe5379f366b477d45c8d0412552da595d
joh-an-ng <me@johan.ng>
2025-05-03 13:28:19 +0200

    Set metadata and playback state in Android MediaInfoManager

--- Maui.Audio.Player/MediaInfoManager/MediaInfoManager.android.cs
+++ Maui.Audio.Player/MediaInfoManager/MediaInfoManager.android.cs
@@ -1,3 +1,4 @@
+using Android.Media;
 using Android.Media.Session;
 using Application = Android.App.Application;
 
@@ -7,7 +8,7 @@ public partial class MediaInfoManager : IMediaInfoManager
 {
     private const string SessionTag = "Maui.Audio.Player.MediaInfoManager";
     
-    private MediaSession _mediaSession;
+    private readonly MediaSession _mediaSession;
     
     public MediaInfoManager()
     {
@@ -24,7 +25,20 @@ public partial class MediaInfoManager : IMediaInfoManager
 
     public void SetMediaInfo(MediaInfo mediaInfo)
     {
+        var metadata = new MediaMetadata.Builder()
+            .PutString(MediaMetadata.MetadataKeyTitle, mediaInfo.Title)
+            ?.PutString(MediaMetadata.MetadataKeyArtist, mediaInfo.Artist)
+            ?.Build();
         
+        var playbackState = new PlaybackState.Builder()
+            .SetActions(PlaybackState.ActionPlay)
+            ?.SetState(PlaybackStateCode.Playing, PlaybackState.PlaybackPositionUnknown, 1)
+            ?.Build();
+        
+        _mediaSession.SetMetadata(metadata);
+        _mediaSession.SetPlaybackState(playbackState);
+        
+        _mediaSession.Active = true;
     }
 
     public void SetPauseCommand(Action action)
