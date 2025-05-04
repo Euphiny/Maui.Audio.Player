ccc252a5b12dfd8c2ad1cf39d23885158e3f6aaa
joh-an-ng <me@johan.ng>
2025-05-04 11:01:12 +0200

    Remove all platform specific implementations of AudioPlayerController

--- Maui.Audio.Player/AudioPlayerController/AudioPlayerController.cs
+++ Maui.Audio.Player/AudioPlayerController/AudioPlayerController.cs
@@ -1,14 +1,25 @@
+using Maui.Audio.Player.AudioPlayer;
 using Maui.Audio.Player.MediaInfoManager;
 
 namespace Maui.Audio.Player.AudioPlayerController;
 
-public partial class AudioPlayerController : IAudioPlayerController
+public class AudioPlayerController : IAudioPlayerController
 {
+    private readonly IMediaInfoManager _mediaInfoManager;
+    
+    private IAudioPlayer? _player;
+    private MediaInfo? _mediaInfo;
+    
     public PlayerInfo PlayerInfo => new(_player?.Duration ?? 0, _player?.Progress ?? 0, _player?.IsPlaying ?? false);
-
+    
+    public event EventHandler? PlaybackEnded;
+    
     public AudioPlayerController(IMediaInfoManager mediaInfoManager)
     {
         _mediaInfoManager = mediaInfoManager;
+
+        _mediaInfoManager.SetPauseCommand(Pause);
+        _mediaInfoManager.SetPlayCommand(Play);
     }
     
     public void Start(string url, MediaInfo mediaInfo)
@@ -22,7 +33,29 @@ public partial class AudioPlayerController : IAudioPlayerController
 
         Play();
     }
+    
+    public void Play()
+    {
+        if (_player == null || _mediaInfo == null)
+            throw new NullReferenceException();
+        
+        _player.Play();
+        _mediaInfoManager.SetMediaInfo(_mediaInfo);
+    }
 
+    public void Pause()
+    {
+        if (_player == null)
+            throw new NullReferenceException();
+        
+        _player.Pause();
+    }
+
+    public void Seek(double positionInSeconds)
+    {
+        _player?.Seek(positionInSeconds);
+    }
+    
     private void Stop()
     {
         if (_player != null)
--- Maui.Audio.Player/AudioPlayerController/AudioPlayerController.net.cs
+++
@@ -1,7 +0,0 @@
-namespace Maui.Audio.Player.AudioPlayerController;
-
-public partial class AudioPlayerController : IAudioPlayerController
-{
-    public PlayerInfo PlayerInfo { get; } = null!;
-    public void Start(string url, MediaInfo mediaInfo) { }
-}
