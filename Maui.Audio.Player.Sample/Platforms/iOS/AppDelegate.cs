0646abf60f15121fce34e398453b0d9929ee15d8
joh-an-ng <me@johan.ng>
2025-05-08 19:54:32 +0200

    Create sample project

---
+++ Maui.Audio.Player.Sample/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Maui.Audio.Player.Sample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
