42fc3eb82c92e6c9399d0b389c2600f517ba8d75
joh-an-ng <me@johan.ng>
2025-05-09 22:10:58 +0200

    Ask user for notification permissions

--- Maui.Audio.Player.Sample/Platforms/Android/MainActivity.cs
+++ Maui.Audio.Player.Sample/Platforms/Android/MainActivity.cs
@@ -1,4 +1,5 @@
-﻿using Android.App;
+﻿using Android;
+using Android.App;
 using Android.Content.PM;
 using Android.OS;
 
@@ -7,4 +8,24 @@ namespace Maui.Audio.Player.Sample;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override async void OnStart()
+    {
+        base.OnStart();
+        
+        PermissionStatus status = await Permissions.RequestAsync<NotificationPermission>();
+    }
+}
+
+public class NotificationPermission : Permissions.BasePlatformPermission
+{
+    public override (string androidPermission, bool isRuntime)[] RequiredPermissions
+    {
+        get
+        {
+            var result = new List<(string androidPermission, bool isRuntime)>();
+            if (OperatingSystem.IsAndroidVersionAtLeast(33))
+                result.Add((Manifest.Permission.PostNotifications, true));
+            return result.ToArray();
+        }
+    }
 }
